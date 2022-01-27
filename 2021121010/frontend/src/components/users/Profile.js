f1d2f179627a55057d6213e48b5ee9454872517a
Shikhar Saxena <56793913+shikhar-saxena@users.noreply.github.com>
2022-01-27 12:59:54 +0530

    Add Profile

--- 2021121010/frontend/src/components/users/Profile.js
+++ 2021121010/frontend/src/components/users/Profile.js
@@ -1,19 +1,59 @@
-import axios from "axios";
+import axios from "../templates/axiosConfig";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Card, CardContent, CardHeader, Grid, Typography } from "@mui/material";
 
-const Profile = (props) => {
-  const [details, setDetails] = useState([]);
+const Profile = ({ choice }) => {
+  const navigate = useNavigate();
+  const [details, setDetails] = useState({});
   useEffect(() => {
     axios
-      .get("http://localhost:4000/vendor/profile") // unimplemented
+      .get(`/${choice}/profile`, {
+        headers: { authorization: localStorage.getItem("authorization") },
+      })
       .then((response) => {
+        // console.log(response.data);
         setDetails(response.data);
       })
-      .catch(function (error) {
-        console.log(error);
+      .catch((error) => {
+        navigate("/login");
       });
   }, []);
-  return <div></div>;
+
+  return (
+    <Grid container align={"center"} spacing={2} key={details["_id"]}>
+      {/* <div>{JSON.stringify(details)}</div> */}
+      <Grid item xs={12} padding={2}>
+        <Typography variant="h4" color="primary">
+          Profile
+        </Typography>
+      </Grid>
+      {Object.entries(details).map((entry) => {
+        let key = entry[0];
+        let value = entry[1];
+
+        if (
+          !(
+            key === "_id" ||
+            key === "password" ||
+            key === "__v" ||
+            key === "wallet"
+          )
+        )
+          return (
+            <Grid item key={key} xs={12} padding={3}>
+              <Typography
+                style={{ fontWeight: 600, textTransform: "uppercase" }}
+                color="primary"
+              >
+                {key}
+              </Typography>
+              <Typography>{value}</Typography>
+            </Grid>
+          );
+      })}
+    </Grid>
+  );
 };
 
 export default Profile;
