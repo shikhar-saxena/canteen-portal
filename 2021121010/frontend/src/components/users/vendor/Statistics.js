f3c18069192ce5a772691962978f4a5cbc4ea62c
Shikhar Saxena <56793913+shikhar-saxena@users.noreply.github.com>
2022-01-27 14:42:18 +0530

    add Statistics

---
+++ 2021121010/frontend/src/components/users/vendor/Statistics.js
@@ -0,0 +1,73 @@
+import { useNavigate } from "react-router-dom";
+import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
+import axios from "../../templates/axiosConfig";
+import { Grid } from "@mui/material";
+
+const Statistics = () => {
+  const [count, setCount] = useState([]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("/vendor/statistics", {
+        headers: { authorization: localStorage.getItem("authorization") },
+      })
+      .then((response) => {
+        setCount(response.data);
+      })
+      .catch((error) => {
+        navigate("/login");
+      });
+  }, []);
+
+  return (
+    <Grid container align={"center"} spacing={2}>
+      <Grid item xs={12} padding={4}>
+        <Typography variant="h4" color="primary">
+          Statistics
+        </Typography>
+      </Grid>
+
+      <Grid item xs={12} padding={4}>
+        <Typography
+          variant="h5"
+          color="primary"
+          style={{ fontWeight: 500, textTransform: "uppercase" }}
+        >
+          Placed Order Count
+        </Typography>
+        <Typography style={{ fontSize: 50 }}>
+          {count.placedOrderCount}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} padding={4}>
+        <Typography
+          variant="h5"
+          color="primary"
+          style={{ fontWeight: 500, textTransform: "uppercase" }}
+        >
+          pending Order Count
+        </Typography>
+        <Typography style={{ fontSize: 50 }}>
+          {count.pendingOrderCount}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} padding={4}>
+        <Typography
+          variant="h5"
+          color="primary"
+          style={{ fontWeight: 500, textTransform: "uppercase" }}
+        >
+          completed Order Count
+        </Typography>
+        <Typography style={{ fontSize: 50 }}>
+          {count.completedOrderCount}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Statistics;
