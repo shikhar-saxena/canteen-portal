d16a9daa4151a36a9efec4de15008badb9a07279
Shikhar Saxena <56793913+shikhar-saxena@users.noreply.github.com>
2022-01-27 11:46:21 +0530

    app.js changes

--- 2021121010/frontend/src/App.js
+++ 2021121010/frontend/src/App.js
@@ -1,15 +1,14 @@
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import "./App.css";
-import UsersList from "./components/users/UsersList";
-import Home from "./components/common/Home";
 import Register from "./components/common/Register";
 import Navbar from "./components/templates/Navbar";
 import BuyerNavbar from "./components/users/buyer/Navbar";
 import VendorNavbar from "./components/users/vendor/Navbar";
-import Profile from "./components/users/Profile";
+// import Profile from "./components/users/Profile";
 import Login from "./components/common/Login";
 import VendorDashboard from "./components/users/vendor/Dashboard";
 import BuyerDashboard from "./components/users/buyer/Dashboard";
+import Bla from "./components/users/vendor/bla";
 
 const Layout = ({ value }) => {
   return (
@@ -46,51 +45,21 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout value={1} />}>
-          <Route path="/" element={<Home />} />
-          <Route path="users" element={<UsersList />} />
+          <Route path="/" element={<Register />} />
           <Route path="register" element={<Register />} />
           <Route path="login" element={<Login />} />
-          <Route path="profile" element={<Profile />} />
         </Route>
 
         <Route path="/buyer" element={<Layout value={2} />}>
           <Route path="/buyer" element={<BuyerDashboard />} />
-          {/*<Route path="users" element={<UsersList />} />
-          <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
-          <Route path="profile" element={<Profile />} /> */}
+          {/* /profile and /orders */}
         </Route>
-        {/* 
-        <Button color="inherit" onClick={() => navigate("/buyer")}>
-          Dashboard
-        </Button>
-        <Button color="inherit" onClick={() => navigate("/buyer/profile")}>
-          Profile
-        </Button>
-        <Button color="inherit" onClick={() => navigate("/buyer/orders")}>
-          My Orders
-        </Button> */}
-
         <Route path="/vendor" element={<Layout value={3} />}>
-          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/vendor" element={<VendorDashboard />} />
-          {/*<Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
-          <Route path="profile" element={<Profile />} /> */}
+          <Route path="/vendor/bla" element={<Bla />} />
+          {/* TODO: */}
+          {/* /profile /orders /statistics */}
         </Route>
-        {/* 
-        <Button color="inherit" onClick={() => navigate("/vendor")}>
-          Dashboard
-        </Button>
-        <Button color="inherit" onClick={() => navigate("/vendor/profile")}>
-          Profile
-        </Button>
-        <Button color="inherit" onClick={() => navigate("/vendor/orders")}>
-          Orders
-        </Button>
-        <Button color="inherit" onClick={() => navigate("/vendor/statistics")}>
-          Statistics
-        </Button> */}
       </Routes>
     </BrowserRouter>
   );
