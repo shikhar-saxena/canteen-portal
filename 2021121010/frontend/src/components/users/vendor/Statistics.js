48609cf5137913ae5715b227d5bd10e8f5bae569
Shikhar Saxena <56793913+shikhar-saxena@users.noreply.github.com>
2022-01-29 20:05:38 +0530

    Add top 5 items

--- 2021121010/frontend/src/components/users/vendor/Statistics.js
+++ 2021121010/frontend/src/components/users/vendor/Statistics.js
@@ -6,6 +6,7 @@ import { Grid } from "@mui/material";
 
 const Statistics = () => {
   const [count, setCount] = useState([]);
+  const [orders, setOrders] = useState([]);
 
   const navigate = useNavigate();
 
@@ -15,12 +16,30 @@ const Statistics = () => {
         headers: { authorization: localStorage.getItem("authorization") },
       })
       .then((response) => {
+        setOrders(response.data.orders);
         setCount(response.data.count);
       })
       .catch((error) => {
         navigate("/login");
       });
   }, []);
+
+  orders.sort((a, b) => {
+    let completedOrdersA, completedOrdersB;
+    completedOrdersA = completedOrdersB = 0;
+
+    for (let i = 0; i < a.length; i++) {
+      if (a[i].status === "COMPLETED") completedOrdersA++;
+    }
+
+    for (let i = 0; i < b.length; i++) {
+      if (b[i].status === "COMPLETED") completedOrdersB++;
+    }
+
+    return completedOrdersB - completedOrdersA;
+  });
+
+  // console.log(JSON.stringify(orders));
 
   return (
     <Grid container align={"center"} spacing={2}>
@@ -65,6 +84,26 @@ const Statistics = () => {
         <Typography style={{ fontSize: 50 }}>
           {count.completedOrderCount}
         </Typography>
+      </Grid>
+      <Grid item xs={12} padding={4}>
+        <Typography
+          variant="h5"
+          color="primary"
+          style={{
+            fontWeight: 500,
+            textTransform: "uppercase",
+            paddingBottom: "0.5rem",
+          }}
+        >
+          top 5 items sold
+        </Typography>
+        {orders.map((orderPerItem) => {
+          return (
+            <Typography style={{ padding: "0.5rem", fontSize: 20 }}>
+              {orderPerItem[0].item.name}
+            </Typography>
+          );
+        })}
       </Grid>
     </Grid>
   );
